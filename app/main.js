d493711a2fcb00c325429bab046c9afec6152f94
Harry Moore <harrymoore@yahoo.com>
2016-07-04 21:54:54 -0400

    new build tasks

--- app/main.js
+++ app/main.js
@@ -9,30 +9,28 @@ var mainWindow = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function () {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
-  // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    // and load the index.html of the app.
+    mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
-  // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
-
-  // Open the devtools.
-  // mainWindow.openDevTools();
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    // Open the devtools.
+    // mainWindow.openDevTools();
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,43 @@
+
+var childProcess = require('child_process'); 
+var electron     = require('electron-prebuilt'); 
+var gulp         = require('gulp'); 
+var jetpack      = require('fs-jetpack'); 
+var usemin       = require('gulp-usemin'); 
+var uglify       = require('gulp-uglify');
+
+var projectDir = jetpack; 
+var srcDir     = projectDir.cwd('./app'); 
+var destDir    = projectDir.cwd('./build');
+
 // get the dependencies
-var gulp        = require('gulp'), 
-    childProcess  = require('child_process'), 
-    electron      = require('electron-prebuilt');
+var gulp        = require('gulp');
+var childProcess  = require('child_process'); 
+var electron      = require('electron-prebuilt');
+
+gulp.task('clean', function (callback) { 
+    return destDir.dirAsync('.', { empty: true }); 
+});
+
+gulp.task('copy', ['clean'], function () { 
+    return projectDir.copyAsync('app', destDir.path(), { 
+        overwrite: true, matching: [ 
+            './node_modules/**/*', 
+            '*.html', 
+            '*.css', 
+            'main.js', 
+            'package.json' 
+       ] 
+    }); 
+});
+
+gulp.task('build', ['copy'], function () { 
+  return gulp.src('./app/index.html') 
+    .pipe(usemin({ 
+      js: [uglify()] 
+    })) 
+    .pipe(gulp.dest('build/')); 
+    }); 
 
 // create the gulp task
 // gulp.task('run', function () { 
